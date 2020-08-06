63b76c84edb70a65036eda0e672520b22a503bb9
Johan Havenga <johan.havenga@gmail.com>
2020-08-06 21:57:55 +0200

    Added ads to results page

--- public/app/views/widgets/timingprovider.php
+++ public/app/views/widgets/timingprovider.php
@@ -11,7 +11,7 @@ switch ($edition_data['timingprovider_id']) {
     case 2:
         ?>
         <div class="content row">
-            <div class="col-lg-5">
+            <div class="col-lg-5 col-sm-5">
                 <p class="text-center">
                     <a href="<?=$url;?>" title="View results on FinishTime site">
                         <img src="/assets/img/FinishTime_Logo.png" alt="FinishTime" style="width: 80%;"/>
@@ -19,7 +19,7 @@ switch ($edition_data['timingprovider_id']) {
                 </p>
             </div>
 
-            <div class="col-lg-7">
+            <div class="col-lg-7 col-sm-7">
                 <h5>Official time keeping done by <a href="<?=$url;?>" title="View results on FinishTime site">
                     <?= $edition_data['timingprovider_name']; ?></a></h5>
                 <p style="font-size: 0.9em;">Click on their logo to view the official results on their site</p>
