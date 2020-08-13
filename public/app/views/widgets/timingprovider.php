fe5bc4b538cce10a0ab04e7c99730cce62082576
Johan Havenga <johan.havenga@gmail.com>
2020-08-13 20:58:52 +0200

    ChampionChip Timekeeping

--- public/app/views/widgets/timingprovider.php
+++ public/app/views/widgets/timingprovider.php
@@ -7,20 +7,39 @@ if (isset($url_list[4][0])) {
     $url=$edition_data['timingprovider_url'];
 }
 // TIMING PROVIDER
-switch ($edition_data['timingprovider_id']) {
+switch ($edition_data['timingprovider_id']) {    
     case 2:
         ?>
         <div class="content row">
             <div class="col-lg-5 col-sm-5">
                 <p class="text-center">
                     <a href="<?=$url;?>" title="View results on FinishTime site">
-                        <img src="/assets/img/FinishTime_Logo.png" alt="FinishTime" style="width: 80%;"/>
+                        <img src="/assets/img/ChampionChipAfrica_Logo.png" alt="FinishTime Logo" style="width: 80%;"/>
                     </a>
                 </p>
             </div>
 
             <div class="col-lg-7 col-sm-7">
                 <h5>Official time keeping done by <a href="<?=$url;?>" title="View results on FinishTime site">
+                    <?= $edition_data['timingprovider_name']; ?></a></h5>
+                <p style="font-size: 0.9em;">Click on their logo to view the official results on their site</p>
+            </div>
+        </div>
+        <?php
+        break;
+    case 3:
+        ?>
+        <div class="content row">
+            <div class="col-lg-5 col-sm-5">
+                <p class="text-center">
+                    <a href="<?=$url;?>" title="View results on ChampionChip site">
+                        <img src="/assets/img/ChampionChipAfrica_Logo.png" alt="ChampionChip Logo" style="width: 80%;"/>
+                    </a>
+                </p>
+            </div>
+
+            <div class="col-lg-7 col-sm-7">
+                <h5>Official time keeping done by <a href="<?=$url;?>" title="View results on ChampionChip site">
                     <?= $edition_data['timingprovider_name']; ?></a></h5>
                 <p style="font-size: 0.9em;">Click on their logo to view the official results on their site</p>
             </div>
