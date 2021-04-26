5b1d71b4e75984686a4014241e9d60d8e9adae10
Johan Havenga <johan.havenga@gmail.com>
2021-04-26 09:34:14 +0200

    Scam block + FAQ changes

--- public/app/controllers/Contact.php
+++ public/app/controllers/Contact.php
@@ -1,32 +1,35 @@
 <?php
 
-class Contact extends Frontend_Controller {
+class Contact extends Frontend_Controller
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
-//    public function test() {
-//        $data = [
-//            "to" => "johan.havenga@gmail.com",
-//            "subject" => "Contact Form TESTé",
-//            "body" => "<p>Hello,</p>"
-//            . "<p>Please see below for an enquiry send from the "
-//            . "<b><a href = 'https://www.roadrunning.co.za/' style = 'color:#222222 !important;text-decoration:underline !important;'>RoadRunning.co.za</a></b> website "
-//            . "by a runner enquiring about the <b>Growthpoint Sundowner 10km 2019</b> race:"
-//            . "<p><b>Name:</b> Johan<br>"
-//            . "<b>Surname:</b> Havenga<br>"
-//            . "<b>Email:</b> johan.havenga@gmail.com<br>"
-//            . "<b>Query:</b> This is a comment ôéêº</p>"
-//            . "<p>View the race listing <a href = 'https://www.roadrunning.co.za/' style = 'color:#222222 !important;text-decoration:underline !important;'>here</a>."
-//            . "<p>Please reply to this email to answer the runner's query.",
-//            "from" => "johnahavenga@woolworths.co.za",
-//            "from_name" => "Johan Hé",
-//        ];
-//        echo $this->set_email($data);
-//    }
+    //    public function test() {
+    //        $data = [
+    //            "to" => "johan.havenga@gmail.com",
+    //            "subject" => "Contact Form TESTé",
+    //            "body" => "<p>Hello,</p>"
+    //            . "<p>Please see below for an enquiry send from the "
+    //            . "<b><a href = 'https://www.roadrunning.co.za/' style = 'color:#222222 !important;text-decoration:underline !important;'>RoadRunning.co.za</a></b> website "
+    //            . "by a runner enquiring about the <b>Growthpoint Sundowner 10km 2019</b> race:"
+    //            . "<p><b>Name:</b> Johan<br>"
+    //            . "<b>Surname:</b> Havenga<br>"
+    //            . "<b>Email:</b> johan.havenga@gmail.com<br>"
+    //            . "<b>Query:</b> This is a comment ôéêº</p>"
+    //            . "<p>View the race listing <a href = 'https://www.roadrunning.co.za/' style = 'color:#222222 !important;text-decoration:underline !important;'>here</a>."
+    //            . "<p>Please reply to this email to answer the runner's query.",
+    //            "from" => "johnahavenga@woolworths.co.za",
+    //            "from_name" => "Johan Hé",
+    //        ];
+    //        echo $this->set_email($data);
+    //    }
 
-    public function index() {
+    public function index()
+    {
         $this->data_to_views['page_title'] = "Contact Us";
         $this->data_to_views['meta_description'] = "Use the form on this page to send us an email";
         $this->data_to_views['form_url'] = '/contact';
@@ -65,35 +68,36 @@ class Contact extends Frontend_Controller {
             redirect(base_url("contact/confirm"));
 
 
-//            $this->data_to_views['mail_id'] = $mail_id;
-//            $this->data_to_views['email'] = $this->input->post('user_email');
-//
-//            $this->session->set_flashdata([
-//                'alert' => "Email has been send",
-//                'status' => "success",
-//            ]);
-//
-//            $this->load->view($this->header_url, $this->data_to_views);
-//            $this->load->view('contact/contact_confirm', $this->data_to_views);
-//            $this->load->view($this->footer_url, $this->data_to_views);
+            //            $this->data_to_views['mail_id'] = $mail_id;
+            //            $this->data_to_views['email'] = $this->input->post('user_email');
+            //
+            //            $this->session->set_flashdata([
+            //                'alert' => "Email has been send",
+            //                'status' => "success",
+            //            ]);
+            //
+            //            $this->load->view($this->header_url, $this->data_to_views);
+            //            $this->load->view('contact/contact_confirm', $this->data_to_views);
+            //            $this->load->view($this->footer_url, $this->data_to_views);
         }
     }
 
     // SEND CONFIRMATION EMAIL
-    private function send_contact_email($email_data) {
+    private function send_contact_email($email_data)
+    {
         // send to info@roadrunning
         $data = [
             "to" => $this->ini_array['email']['from_address_server'],
             "body" => "<h3>Contact form</h3><p>"
-            . "<b>Name:</b> " . $email_data['user_name'] . " " . $email_data['user_surname'] . "<br>"
-            . "<b>Email:</b> " . $email_data['user_email'] . "</p>"
-            . "<p style='padding-left: 15px; padding-bottom:0; margin: 20px 0; border-left: 4px solid #ccc;'><b>Comment:</b><br>" . nl2br($email_data['user_message']) . "</p>",
+                . "<b>Name:</b> " . $email_data['user_name'] . " " . $email_data['user_surname'] . "<br>"
+                . "<b>Email:</b> " . $email_data['user_email'] . "</p>"
+                . "<p style='padding-left: 15px; padding-bottom:0; margin: 20px 0; border-left: 4px solid #ccc;'><b>Comment:</b><br>" . nl2br($email_data['user_message']) . "</p>",
             "subject" => "Enquiry from RoadRunning.co.za #" . uniqid(),
             "from" => $email_data['user_email'],
             "from_name" => $email_data['user_name'] . " " . $email_data['user_surname'],
         ];
-//        echo $data['body'];
-//        wts($data,1);
+        //        echo $data['body'];
+        //        wts($data,1);
         $this->set_email($data);
 
         // send mail to user
@@ -103,7 +107,8 @@ class Contact extends Frontend_Controller {
         return true;
     }
 
-    public function event($slug = "") {
+    public function event($slug = "")
+    {
         $this->load->model('edition_model');
         $edition_sum = $this->edition_model->get_edition_id_from_slug($slug);
 
@@ -119,8 +124,8 @@ class Contact extends Frontend_Controller {
 
         $this->data_to_views['crumbs_arr'][$slug] = $this->data_to_views['cancel_url'];
 
-//        wts($edition_sum);
-//        wts($edition_data, true);
+        //        wts($edition_sum);
+        //        wts($edition_data, true);
 
         $this->data_to_views['page_title'] = "Contact Event Organisers";
         $this->data_to_views['form_url'] = $this->data_to_views['error_url'] = '/contact/event/' . $slug;
@@ -130,7 +135,9 @@ class Contact extends Frontend_Controller {
         $this->form_validation->set_rules('user_surname', 'Surname', 'trim|required');
         $this->form_validation->set_rules('user_email', 'email address', 'trim|required|valid_email');
         $this->form_validation->set_rules('user_message', 'Message', 'trim|required');
-        $this->form_validation->set_rules('g-recaptcha-response', 'Captcha', 'callback_recaptcha');
+        if ($this->ini_array['enviroment']['server'] == "production") {
+            $this->form_validation->set_rules('g-recaptcha-response', 'Captcha', 'callback_recaptcha');
+        }
 
         // load correct view
         if ($this->form_validation->run() === FALSE) {
@@ -139,11 +146,17 @@ class Contact extends Frontend_Controller {
             $this->load->view('contact/event', $this->data_to_views);
             $this->load->view($this->footer_url, $this->data_to_views);
         } else {
-            // set user_data from post
-            foreach ($this->input->post() as $field => $value) {
-                $email_data[$field] = $value;
-            }
-            $this->send_event_email($email_data, $this->data_to_views['edition_data']);
+            // find spam using the string STOLEN IMAGE
+            $pos = strpos($this->input->post("user_message"), "stolen image");
+            if ($pos === false) {                
+                // set user_data from post
+                foreach ($this->input->post() as $field => $value) {
+                    $email_data[$field] = $value;
+                }
+                $this->send_event_email($email_data, $this->data_to_views['edition_data']);
+            } else {
+                // silent fail
+            }            
 
             $this->session->set_flashdata([
                 'alert' => "Your email has been send to the organisers",
@@ -159,21 +172,22 @@ class Contact extends Frontend_Controller {
     }
 
     // SEND EVENT EMAIL
-    private function send_event_email($email_data, $edition_data) {
+    private function send_event_email($email_data, $edition_data)
+    {
         $data = [
-//            "to" => $edition_data['user_email'].",".$this->ini_array['email']['from_address_server'],
+            //            "to" => $edition_data['user_email'].",".$this->ini_array['email']['from_address_server'],
             "to" => $edition_data['user_email'],
             "cc" => $this->ini_array['email']['from_address_server'],
             "subject" => $edition_data['annual_name'] . " - Enquiry from RoadRunning.co.za #" . uniqid(),
             "body" => "<p>Hello,</p>"
-            . "<p>Please see below for an enquiry send from the "
-            . "<a href = 'https://www.roadrunning.co.za/' style = 'color:#222222 !important;text-decoration:underline !important;'>RoadRunning.co.za</a> website "
-            . "by a runner enquiring about the <b>" . $edition_data['edition_name'] . "</b> race:"
-            . "<p><b>Name:</b> " . $email_data['user_name'] . " " . $email_data['user_surname'] . "<br>"
-            . "<b>Email:</b> " . $email_data['user_email'] . "</p>"
-            . "<p style='padding-left: 15px; padding-bottom: 0; margin: 20px 0; border-left: 4px solid #ccc;'><b>Query:</b><br> " . nl2br($email_data['user_message']) . "</p>"
-            . "<p>View the race listing <a href = 'https://www.roadrunning.co.za/event/" . $edition_data['edition_slug'] . "' style = 'color:#222222 !important;text-decoration:underline !important;'>here</a>."
-            . "<br>Please reply to this email to answer the runner's query.</p>",
+                . "<p>Please see below for an enquiry send from the "
+                . "<a href = 'https://www.roadrunning.co.za/' style = 'color:#222222 !important;text-decoration:underline !important;'>RoadRunning.co.za</a> website "
+                . "by a runner enquiring about the <b>" . $edition_data['edition_name'] . "</b> race:"
+                . "<p><b>Name:</b> " . $email_data['user_name'] . " " . $email_data['user_surname'] . "<br>"
+                . "<b>Email:</b> " . $email_data['user_email'] . "</p>"
+                . "<p style='padding-left: 15px; padding-bottom: 0; margin: 20px 0; border-left: 4px solid #ccc;'><b>Query:</b><br> " . nl2br($email_data['user_message']) . "</p>"
+                . "<p>View the race listing <a href = 'https://www.roadrunning.co.za/event/" . $edition_data['edition_slug'] . "' style = 'color:#222222 !important;text-decoration:underline !important;'>here</a>."
+                . "<br>Please reply to this email to answer the runner's query.</p>",
             "from" => $email_data['user_email'],
             "from_name" => $email_data['user_name'] . " " . $email_data['user_surname'],
         ];
@@ -183,7 +197,8 @@ class Contact extends Frontend_Controller {
     }
 
     // GENERIC CONFIRMATION PAGE
-    function confirm() {
+    function confirm()
+    {
         if ($this->session->flashdata('confirm_msg') !== null) {
             $this->load->view($this->header_url, $this->data_to_views);
             $this->load->view($this->notice_url, $this->data_to_views);
@@ -193,5 +208,4 @@ class Contact extends Frontend_Controller {
             redirect(base_url());
         }
     }
-
 }
--- public/app/views/main/faq.php
+++ public/app/views/main/faq.php
@@ -4,10 +4,26 @@
             <!-- Content-->
             <div class="content col-lg-9">
                 <h3 class="text-uppercase"><?= $page_title; ?></h3>
-                <p>Got a question. Check if it is asked and answered below, else <a href="<?= base_url("contact"); ?>">ask me</a> something.<p>
+                <p>Got a question. Check if it is asked and answered below, else <a href="<?= base_url("contact"); ?>">ask me</a> something.
+                <p>
                 <div class="accordion accordion-shadow">
                     <div class="ac-item">
-                        <h5 class="ac-title">   
+                        <h5 class="ac-title">
+                            <i class="fa fa-question-circle" aria-hidden="true"></i>
+                            How to virtual races work?
+                        </h5>
+                        <div class="ac-content">
+                            <p>They all work a little differently from one another, but the basics are mostly the same:<br>
+                            You enter a race distance online. Then you run the distance you entered in the date range 
+                            required on your own route.<br>
+                            You time yourself and send proof of the run in to the organisers.<br>
+                            You generally have a few days in which you need to run the race distance in one go.<br>
+                            And that is it. Most have a medal they courier to you, but please check individual race 
+                            pages for more information.</p>
+                        </div>
+                    </div>
+                    <div class="ac-item">
+                        <h5 class="ac-title">
                             <i class="fa fa-question-circle" aria-hidden="true"></i>
                             There are 2 numbers in my race pack. Which goes where?
                         </h5>
@@ -16,17 +32,19 @@
                         </div>
                     </div>
                     <div class="ac-item">
-                        <h5 class="ac-title">   
+                        <h5 class="ac-title">
                             <i class="fa fa-question-circle" aria-hidden="true"></i>
                             What do I do with the indemnity declaration slip?
                         </h5>
                         <div class="ac-content">
-                            <p>You need to complete it and hand it back to the organisers at a registration point. If you don't know where this is, 
+                            <p>You need to complete it and hand it back to the organisers at a registration point. If you don't know where this is,
                                 complete the slip and fold it in under the number</p>
                         </div>
                     </div>
-                    <div class="ac-item <?php if ($open=='license') { echo 'ac-active'; } ?>" id="license">
-                        <h5 class="ac-title">   
+                    <div class="ac-item <?php if ($open == 'license') {
+                                            echo 'ac-active';
+                                        } ?>" id="license">
+                        <h5 class="ac-title">
                             <i class="fa fa-question-circle" aria-hidden="true"></i>
                             What does it mean to be a "licensed athlete"?
                         </h5>
@@ -39,24 +57,24 @@
                         </div>
                     </div>
                     <div class="ac-item">
-                        <h5 class="ac-title">   
+                        <h5 class="ac-title">
                             <i class="fa fa-question-circle" aria-hidden="true"></i>
                             Are we the organisers of all these races?
                         </h5>
                         <div class="ac-content">
                             <p>
-                                No we are not. In fact, we do not organise any events. All the events are organisers by different clubs and other event organisers. 
+                                No we are not. In fact, we do not organise any events. All the events are organisers by different clubs and other event organisers.
                                 We gather the information from everyone and post the events in a uniform manner.</p>
                         </div>
                     </div>
                     <div class="ac-item">
-                        <h5 class="ac-title">   
+                        <h5 class="ac-title">
                             <i class="fa fa-question-circle" aria-hidden="true"></i>
                             How do I enter races?
                         </h5>
                         <div class="ac-content">
                             <p>
-                                It depends on the event, especially the size of the event. The smaller races will only do on-the-day entries. The really big 
+                                It depends on the event, especially the size of the event. The smaller races will only do on-the-day entries. The really big
                                 events will again only do online entries to ease the admin and make sure tracking chips are registered correctly.
                                 Please make sure you read the information around entries and registration carefully to avoid disappointment.</p>
                         </div>
@@ -66,7 +84,7 @@
             <!-- end: Content-->
 
             <!-- Sidebar-->
-            <div class="sidebar col-lg-3">  
+            <div class="sidebar col-lg-3">
                 <?php
                 // ADS WIDGET
                 $this->load->view('widgets/side_ad');
